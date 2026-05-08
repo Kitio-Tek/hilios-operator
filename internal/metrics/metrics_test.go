a1ae04a693810443baf0590da82ab1d7d3e8bc37
Kitio-Tek <kmyriantek@gmail.com>
2026-05-08 02:58:13 +0200

    Add unit tests for metrics counters and gauges

---
+++ internal/metrics/metrics_test.go
@@ -0,0 +1,42 @@
+/*
+Copyright 2026.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package metrics
+
+import (
+	"testing"
+
+	"github.com/prometheus/client_golang/prometheus/testutil"
+)
+
+func TestPolicyEvaluationsCounter(t *testing.T) {
+	t.Parallel()
+	PolicyEvaluationsTotal.Reset()
+	PolicyEvaluationsTotal.WithLabelValues("ns", "p").Inc()
+	PolicyEvaluationsTotal.WithLabelValues("ns", "p").Inc()
+	if got := testutil.ToFloat64(PolicyEvaluationsTotal.WithLabelValues("ns", "p")); got != 2 {
+		t.Fatalf("counter want 2, got %v", got)
+	}
+}
+
+func TestRebalanceSkewGauge(t *testing.T) {
+	t.Parallel()
+	RebalanceSkew.Reset()
+	RebalanceSkew.WithLabelValues("ns", "c").Set(3)
+	if got := testutil.ToFloat64(RebalanceSkew.WithLabelValues("ns", "c")); got != 3 {
+		t.Fatalf("gauge want 3, got %v", got)
+	}
+}
