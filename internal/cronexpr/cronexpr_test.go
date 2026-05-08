032d7734d603edbda1ffcd8c69ff90b7e529217a
Kitio-Tek <kmyriantek@gmail.com>
2026-05-08 13:25:19 +0200

    Test cronexpr Parse/Expr round-trips minute 3

--- internal/cronexpr/cronexpr_test.go
+++ internal/cronexpr/cronexpr_test.go
@@ -126,3 +126,15 @@ func TestParseAndExpr_2(t *testing.T) {
 		t.Fatalf("Expr roundtrip: got %s want %s", s.Expr(), expr)
 	}
 }
+
+func TestParseAndExpr_3(t *testing.T) {
+	t.Parallel()
+	expr := "3 * * * *"
+	s, err := Parse(expr)
+	if err != nil {
+		t.Fatalf("parse: %v", err)
+	}
+	if s.Expr() != expr {
+		t.Fatalf("Expr roundtrip: got %s want %s", s.Expr(), expr)
+	}
+}
